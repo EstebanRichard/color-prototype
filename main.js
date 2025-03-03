02f0b01b22ae296cc756cac8190ffcd9b7f2ac09
Esteban Richard <esteban.richard@live.ca>
2025-03-03 13:23:27 -0500

    .

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
-const button = document.getElementById('addColorButton');
+const button = document.getElementById("addColorButton");
 
-button.addEventListener('click', () => {
-    const section = document.createElement('section');    
-    const randomColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    section.style.backgroundColor = randomColor;
-    section.style.height = '100vh';
-    
-    const palette = document.createElement('div');
+button.addEventListener("click", () => {
+  if (button.id === "addColorButton") {
+    // Création du container des couleurs
+    const palette = document.createElement("div");
     palette.id = "palette";
-    palette.appendChild(section);
-    
     document.body.appendChild(palette);
-    
-    const button = document.createElement('button');
 
+    // Génération aléatoire de la première couleur
+    const colorSection = document.createElement("section");
+    const randomColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    colorSection.style.backgroundColor = randomColor;
+    colorSection.style.height = "100vh";
+    palette.appendChild(colorSection);
 
+    // Sélecteur de couleur
+    const colorInput = document.createElement("input");
+    colorInput.type = "color";
+    colorInput.value = randomColor;
+    colorInput.addEventListener("input", (e) => {
+      colorSection.style.backgroundColor = e.target.value;
+    });
+    colorSection.appendChild(colorInput);
+    palette.appendChild(colorSection);
 
+    // Création du nouveau bouton
+    button.innerText = "+";
+    button.id = "addMoreColorButton";
+  } else if (button.id === "addMoreColorButton") {
+    // Génération aléatoire (inclure un algorithme en React + tard) des autres couleurs
+    const colorSection = document.createElement("section");
+    const randomColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    colorSection.style.backgroundColor = randomColor;
+    colorSection.style.height = "100vh";
+    palette.appendChild(colorSection);
 
-    button.remove();
+    // Sélecteur de couleur
+    const colorInput = document.createElement("input");
+    colorInput.type = "color";
+    colorInput.value = randomColor;
+    colorInput.addEventListener("input", (e) => {
+      colorSection.style.backgroundColor = e.target.value;
+    });
+    colorSection.appendChild(colorInput);
+    palette.appendChild(colorSection);
+  }
+  palette.appendChild(button);
 });
